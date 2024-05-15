c5c749c5b5c6432ae54313b0a67ede8dd6545842
BrenoJLeal <140741784+BrenoJLeal@users.noreply.github.com>
2024-05-15 19:51:57 -0300

    git commit -m "feat: deleção do programa java anterior"

--- SantaClara/src/main/java/IngressoInter.java
+++
@@ -1,4 +0,0 @@
-public interface IngressoInter {
-    void setStatusIngresso(String status);
-    String getStatusIngresso();
-}
--- SantaClara/src/main/java/TipoAssento.java
+++
@@ -1,4 +0,0 @@
-public enum TipoAssento {
-    PLATEIA,
-    CAMAROTE;
-}
