ba592483044c7e9d7f0e54678967152cec0cb793
BrenoJLeal <140741784+BrenoJLeal@users.noreply.github.com>
2024-05-16 14:49:45 -0300

    Update teatrosc.sql

--- teatrosc.sql
+++ teatrosc.sql
@@ -1,13 +1,6 @@
 -- Cria o banco de dados teatrosc
 CREATE DATABASE teatrosc ON (NAME = teatrosc, FILENAME = 'C:\SQL\teatrosc\teatrosc.mdf');
 
--- Utiliza o banco de dados teatrosc
-USE teatrosc;
-ALTER TABLE espetaculos
-ADD elenco VARCHAR(100) NOT NULL DEFAULT '';
-ALTER TABLE espetaculos
-ADD data_exibicao DATE;
-
 
 
 -- Cria a tabela espetaculos
@@ -19,7 +12,7 @@ CREATE TABLE espetaculos (
     descricao TEXT NOT NULL
 );
 
--- Cria a tabela programacao para armazenar os dias de exibiÁ„o de cada espet·culo
+-- Cria a tabela programacao para armazenar os dias de exibi√ß√£o de cada espet√°culo
 CREATE TABLE programacao (
     id INT PRIMARY KEY IDENTITY,
     espetaculo_id INT NOT NULL,
@@ -31,7 +24,7 @@ CREATE TABLE programacao (
 CREATE TABLE assentos (
     id CHAR(4) PRIMARY KEY,
     tipo_assento VARCHAR(15) NOT NULL CHECK (tipo_assento IN ('Plateia', 'Camarote')), -- Pode ser 'Plateia' ou 'Camarote'
-    status VARCHAR(15) NOT NULL DEFAULT 'DisponÌvel' -- Pode ser 'DisponÌvel' ou 'Vendido'
+    status VARCHAR(15) NOT NULL DEFAULT 'Dispon√≠vel' -- Pode ser 'Dispon√≠vel' ou 'Vendido'
 );
 
 -- Cria a tabela ingressos
@@ -40,7 +33,7 @@ CREATE TABLE ingressos (
     programacao_id INT NOT NULL,
     assento_id CHAR(4) NOT NULL,
     tipo_ingresso VARCHAR(15) NOT NULL CHECK (tipo_ingresso IN ('Meio', 'Inteiro', 'Social')), -- Pode ser 'Meio', 'Inteiro' ou 'Social'
-    status VARCHAR(15) NOT NULL DEFAULT 'DisponÌvel', -- Pode ser 'DisponÌvel' ou 'Vendido'
+    status VARCHAR(15) NOT NULL DEFAULT 'Dispon√≠vel', -- Pode ser 'Dispon√≠vel' ou 'Vendido'
     FOREIGN KEY (programacao_id) REFERENCES programacao(id),
     FOREIGN KEY (assento_id) REFERENCES assentos(id)
 );
@@ -52,17 +45,4 @@ CREATE TABLE vendas (
     data_venda DATETIME NOT NULL DEFAULT GETDATE(),
     FOREIGN KEY (ingresso_id) REFERENCES ingressos(id)
 );
--- Cria a trigger para atualizar o status do assento quando um ingresso for comprado
-CREATE TRIGGER UpdateAssentoStatus
-ON vendas
-AFTER INSERT
-AS
-BEGIN
-    -- Atualiza o status do assento associado ao ingresso comprado para 'Ocupado'
-    UPDATE assentos
-    SET status = 'Ocupado'
-    FROM inserted AS i
-    INNER JOIN ingressos AS ing ON i.ingresso_id = ing.id
-    WHERE assentos.id = ing.assento_id;
-END;
-SELECT * FROM sys.triggers;
+
