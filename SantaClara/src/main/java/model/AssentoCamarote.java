ee6965c84f4a3eb433a611f560ec1f69c8808e34
Clara Ferraz <claraferraz@proton.me>
2024-05-24 14:26:17 -0300

    Sprint-2]

--- SantaClara/src/main/java/model/AssentoCamarote.java
+++ SantaClara/src/main/java/model/AssentoCamarote.java
@@ -42,21 +42,18 @@ public class AssentoCamarote extends Assentos {
     };
     @Override
     public void reservar(String r){
+        Boolean disponivel = false;
         for (int i = 0; i < lugares.length; i++) {
             for (int j = 0; j < lugares.length; j++) {
                 if (r.equalsIgnoreCase(this.lugares[i][j])) {
-                    System.out.println("Deseja reservar " + this.lugares[i][j] + "? Sim(1) / Não (0)");
-                    int confirma = sc.nextInt();
-                    if (confirma == 1){
+                        disponivel = true;
                         this.lugares[i][j] = "X ";
-                        this.printLugares();
-                    }
-                    return;
-                } else {
-                    System.out.println("Assento indisponível, por favor escolha outro");
-                    return;
+
                 }
             }
+        }
+        if (!disponivel) {
+            System.out.println("Assento indisponível, por favor escolha outro");
         }
     };
 
