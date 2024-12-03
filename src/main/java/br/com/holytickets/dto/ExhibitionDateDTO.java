2592ad96899d753f7e840489dcb47ad806aa9da7
Breno Leal <copywriting.leal@gmail.com>
2024-12-03 16:37:33 -0300

    fix: update ScheduleService

--- src/main/java/br/com/holytickets/dto/ExhibitionDateDTO.java
+++ src/main/java/br/com/holytickets/dto/ExhibitionDateDTO.java
@@ -3,9 +3,11 @@ package br.com.holytickets.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExhibitionDateDTO {
     @Schema(example = "dd/mm/aaaa HH:MM")
     private String exhibitionDate;
